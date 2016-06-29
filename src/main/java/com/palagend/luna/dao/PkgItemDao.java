2f158d0a1789f2033b58b2d54ef936e7b46b4c91
Bran Stark <palagend@163.com>
2016-06-29 15:56:23 +0800

    update .gitignore

--- src/main/java/com/palagend/luna/dao/PkgItemDao.java
+++
@@ -1,9 +0,0 @@
-package com.palagend.luna.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.palagend.luna.po.PkgItem;
-
-public interface PkgItemDao extends JpaRepository<PkgItem, Integer>{
-	
-}
